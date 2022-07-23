691044f68b0bead1497398b59b6778fd6523d7e5
Luca Porcelli <87008874+Lukasxs93@users.noreply.github.com>
2022-07-23 22:49:22 +0100

    humidity icon

--- weather-app/src/components/Humidity.js
+++ weather-app/src/components/Humidity.js
@@ -1,10 +1,23 @@
-import { Card, Icon } from "semantic-ui-react";
+
+import { Card} from "semantic-ui-react";
+import { BsDropletHalf, BsDroplet, BsDropletFill } from "react-icons/bs";
 
 const Humidity = ({ weatherData }) => {
+	
+	const handleHumidity=(weatherData)=>{
+		if(weatherData.current.humidity<=10){
+        return <BsDroplet/>
+		}else if(weatherData.current.humidity<= 50){
+        return<BsDropletHalf/>
+		}else{
+		return <BsDropletFill/>
+		}
+		
+	}
 	return (
 		<Card.Meta>
 			<label>
-				<Icon name="theme" />
+				{handleHumidity(weatherData)}
 				{weatherData.current.humidity} %{" "}
 			</label>
 		</Card.Meta>
